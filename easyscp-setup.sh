637ccec9bd72d89d6af69242b6a8b9268b52601d
Markus Szywon <markus.szywon@shadowjumper.de>
2014-03-25 21:17:41 +0100

    Add: Oracle Linux support

--- easyscp-setup.sh
+++ easyscp-setup.sh
@@ -21,7 +21,9 @@ echo "2 = Debian"
 echo ""
 echo "3 = OpenSuse"
 echo ""
-echo "4 = Ubuntu"
+echo "4 = Oracle Linux"
+echo ""
+echo "5 = Ubuntu"
 echo ""
 
 while :
@@ -40,7 +42,11 @@ do
         Auswahl="OpenSuse"
     fi
 
-    if [ "$Name" = "4" ] || [ "$Name" = "Ubuntu" ]; then
+    if [ "$Name" = "4" ] || [ "$Name" = "Oracle Linux" ]; then
+        Auswahl="Oracle"
+    fi
+
+    if [ "$Name" = "5" ] || [ "$Name" = "Ubuntu" ]; then
         Auswahl="Ubuntu"
     fi
 
@@ -217,6 +223,102 @@ do
 			# echo "Using OpenSuse. Please wait."
 			echo "Easy Setup currently does not support OpenSuse."
 			# break
+			;;
+		Oracle)
+			echo "Using Oracle Linux. Please wait."
+
+			echo "Build the Software"
+			cd $(dirname $0)"/"
+			make -f Makefile.$Auswahl install > /dev/null
+
+			echo "Copy required files to your system"
+			cp -RLf /tmp/easyscp/* / > /dev/null
+
+			if [ ! -f /etc/easyscp/EasySCP_Config.xml ]; then
+				cp /etc/easyscp/tpl/EasySCP_Config.xml /etc/easyscp/EasySCP_Config.xml
+			fi
+
+			while :
+			do
+				read -p "Secure your mysql installation [Y/N]?" MySQL
+				case "$MySQL" in
+					[JjYy])
+						#echo "ja"
+						mysql_secure_installation
+						break
+						;;
+					[Nn])
+						#echo "nein"
+						break
+						;;
+					*)
+						echo "Wrong selection"
+
+						;;
+				esac
+			done
+
+			echo "Clean the temporary folders"
+			# rm -fR /tmp/easyscp/
+
+			echo "Prepare system config"
+
+			TIMEZONE=`cat /etc/sysconfig/clock | sed "s/^[^\"]*\"//" | sed  "s/\".*//"`
+			echo $TIMEZONE > /etc/timezone
+			TIMEZONE=$(cat /etc/timezone | sed "s/\//\\\\\//g")
+			sed -i".bak" "s/^\;date\.timezone.*$/date\.timezone = \"${TIMEZONE}\" /g" /etc/php.ini
+			# TIMEZONE=$((cat /etc/sysconfig/clock) | sed 's/^[^"]*"//' | sed  's/".*//')
+			# sed -i".bak" "s/^\;date\.timezone.*$/date\.timezone = \"${TIMEZONE}\" /g" /etc/php.ini
+
+			#touch /var/log/rkhunter.log
+			#chmod 644 /var/log/rkhunter.log
+
+			chmod 0700 -R /var/www/easyscp/daemon/
+
+			chmod 0777 /var/www/setup/theme/templates_c/
+			chmod 0777 /var/www/setup/config.xml
+
+			chcon --reference /etc/httpd/conf.d /etc/httpd/vhost.d
+			if [ ! -f /etc/httpd/conf.d/vhost.conf ]; then
+				echo "Include vhost.d/*.conf" >>/etc/httpd/conf.d/vhost.conf
+   			fi
+			chcon --reference /etc/httpd/conf.d/README /etc/httpd/conf.d/vhost.conf
+
+			while :
+			do
+				read -p "Configure iptables [Y/N]? (if unsure, select no)" IPTables
+				case "$IPTables" in
+					[JjYy])
+						#echo "ja"
+						iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited
+						iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+						iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
+						break
+						;;
+					[Nn])
+						#echo "nein"
+						break
+						;;
+					*)
+						echo "Wrong selection"
+
+						;;
+				esac
+			done
+
+			service httpd restart
+			# /etc/init.d/httpd restart
+
+			echo "Starting EasySCP Controller"
+			/etc/init.d/easyscp_control start > /dev/null
+
+			echo "Starting EasySCP Daemon"
+			/etc/init.d/easyscp_daemon start > /dev/null
+
+			echo ""
+			echo "To finish Setup, please enter 'http://YOUR_DOMAIN/setup' into your Browser"
+
+			break
 			;;
 		*)
 			echo "Please type a number or the name of your distribution!"
