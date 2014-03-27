c282a4b2a804ba8a7c93d3600c326f05e8eb2af3
Markus Szywon <markus.szywon@shadowjumper.de>
2014-03-27 17:36:22 +0100

    Update: phpMyAdmin to version 4.1.12

--- gui/tools/pma/libraries/Config.class.php
+++ gui/tools/pma/libraries/Config.class.php
@@ -114,7 +114,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.1.11');
+        $this->set('PMA_VERSION', '4.1.12');
         /**
          * @deprecated
          */
