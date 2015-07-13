466ebe94c5e1c0b78d1a4f30bf9f1849c8bf3be4
Markus Szywon <markus.szywon@shadowjumper.de>
2015-07-13 21:37:39 +0200

    phpMyAdmin to version 4.0.10.10

--- gui/tools/pma/libraries/Config.class.php
+++ gui/tools/pma/libraries/Config.class.php
@@ -102,7 +102,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.0.10.9');
+        $this->set('PMA_VERSION', '4.0.10.10');
         /**
          * @deprecated
          */
@@ -716,8 +716,6 @@ class PMA_Config
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
         curl_setopt($ch, CURLOPT_USERAGENT, 'phpMyAdmin/' . PMA_VERSION);
         curl_setopt($ch, CURLOPT_TIMEOUT, 5);
