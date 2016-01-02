eda52899b64e64b15a24fa996e0a59eb2eaa0b3b
Markus Szywon <markus.szywon@shadowjumper.de>
2016-01-02 12:38:05 +0100

    Improved: cronjob settings and add cronjobs on setup

--- configs/common/database/database.sql
+++ configs/common/database/database.sql
@@ -118,7 +118,7 @@ INSERT INTO `cronjobs` (`user_id`, `schedule`, `command`, `active`, `description
 (1, '0,30 * * * *', 'umask 027; /var/www/easyscp/daemon/CronServerTraffic.php > /var/log/easyscp/CronServerTraffic.log', 'yes', 'Server Traffic', 'Server Traffic', 'root', 'ok', NULL),
 (1, '0,30 * * * *', 'umask 027; /var/www/easyscp/daemon/CronDomainTraffic.php > /var/log/easyscp/CronDomainTraffic.log', 'yes', 'Domain Traffic', 'Domain Traffic', 'root', 'ok', NULL),
 (1, '@daily', 'umask 027; /var/www/easyscp/daemon/CronDomainBackup.php > /var/log/easyscp/CronDomainBackup.log', 'yes', 'Backup for all the customers'' data depending of the domain properties', 'Domain backup', 'root', 'ok', NULL),
-(1, '@daily', 'umask 027; /var/www/easyscp/daemon/CronSystemBackup.php > /var/log/easyscp/CronSystemBackup.log', 'yes', 'Backup for all the customers'' data depending of the domain properties', 'System backup', 'root', 'ok', NULL),
+(1, '@daily', 'umask 027; /var/www/easyscp/daemon/CronSystemBackup.php > /var/log/easyscp/CronSystemBackup.log', 'yes', 'Backup for EasySCP database and all EasySCP configuration files', 'System backup', 'root', 'ok', NULL),
 (1, '0 */12 * * *', '/usr/bin/rkhunter --cronjob --createlogfile /var/log/rkhunter.log.root --display-logfile 1>/var/log/rkhunter.log 2>/dev/null', 'yes', 'Rootkit Hunter', 'Rootkit Hunter', 'root', 'ok', NULL);
 
 -- --------------------------------------------------------
